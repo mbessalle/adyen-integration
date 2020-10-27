8f625786e82a7cef8f2f7aadbf226b0e1c264ac1
Moises Bessalle <mbessalle@gmail.com>
2020-10-27 21:16:45 +0100

    added getClientKey route

--- server.js
+++ server.js
@@ -5,4 +5,13 @@ const axios = require("axios").default;
 const { v1: uuidv1 } = require("uuid");
 require("dotenv").config();
 
+//body parser middleware
 app.use(express.json());
+
+app.get("/getClientKey", (_, res) => {
+  res.send(process.env.CLIENT_KEY);
+});
+
+app.listen(port, () => {
+  console.log(`Backend listening at http://localhost:${port}`);
+});
