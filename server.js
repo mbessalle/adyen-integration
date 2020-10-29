b4956726039c92e4cfd1c020b63776a92d04dfee
Moises Bessalle <mbessalle@gmail.com>
2020-10-29 21:27:47 +0100

    added  drop-in component / paymentMethods route

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 const app = express();
 const port = 5000;
@@ -12,6 +13,34 @@ app.get("/getClientKey", (_, res) => {
   res.send(process.env.CLIENT_KEY);
 });
 
+app.post("/paymentMethods", (req, res) => {
+  axios
+    .post(
+      "https://checkout-test.adyen.com/v64/paymentMethods",
+      {
+        merchantAccount: process.env.MERCHANT_ACCOUNT,
+        countryCode: "NL",
+        amount: {
+          currency: "EUR",
+          value: req.body.value,
+        },
+        channel: "Web",
+        shopperLocale: "nl-NL",
+      },
+      {
+        headers: {
+          "x-API-key": process.env.API_KEY,
+          "content-type": "application/json",
+        },
+      }
+    )
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.error("error 1", error);
+    });
+});
 app.listen(port, () => {
   console.log(`Backend listening at http://localhost:${port}`);
 });
--- src/App.js
+++ src/App.js
@@ -1,19 +1,80 @@
 import React from "react";
 import "./App.css";
 import axios from "axios";
+import AdyenCheckout from "@adyen/adyen-web";
+import '@adyen/adyen-web/dist/adyen.css';
+
+const showFinalResult = (response) => {
+  console.log("final result");
+  console.log(response);
+};
 
 const main = async () => {
-  let clientKey = await axios.get("/getClientKey");
-  // let paymentMethods = axios.post("/paymentMethods", {
-  //   value: 1000,
-  //   currency: "euro",
-  // });
-  console.log(clientKey)
+  let clientKey = axios.get("/getClientKey");
+  let paymentMethods = axios.post("/paymentMethods", {
+    value: 1000,
+    currency: "euro",
+  });
+  [clientKey, paymentMethods] = await Promise.all([clientKey, paymentMethods]);
+  clientKey = clientKey.data;
+
+  const configuration = {
+    paymentMethodsResponse: paymentMethods.data,
+    clientKey: clientKey,
+    locale: "nl-NL",
+    environment: "test",
+    onSubmit: (state, dropin) => {
+      axios
+        .post("/payments", state.data)
+        .then((response) => {
+          if (response.action) {
+            dropin.handleAction(response.action);
+          } else {
+            // Your function to show the final result to the shopper
+            showFinalResult(response);
+          }
+        })
+        .catch((error) => {
+          throw Error(error);
+        });
+    },
+    onAdditionalDetails: (state, dropin) => {
+      // Your function calling your server to make a `/payments/details` request
+      axios
+        .post("/payments/details", state.data)
+        .then((response) => {
+          if (response.action) {
+            dropin.handleAction(response.action);
+          } else {
+            // Your function to show the final result to the shopper
+            showFinalResult(response);
+          }
+        })
+        .catch((error) => {
+          throw Error(error);
+        });
+    },
+    paymentMethodsConfiguration: {
+      card: {
+        hasHolderName: true,
+        holderNameRequired: true,
+        enableStoreDetails: true,
+        hideCVC: false,
+        name: "Credit or debit card",
+      },
+    },
+  };
+  const checkout = new AdyenCheckout(configuration);
+  checkout.create("dropin").mount("#dropin-container");
 };
 main();
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div id="payment-wrapper">
+      <div id="dropin-container"></div>
+    </div>
+  );
 }
 
 export default App;
